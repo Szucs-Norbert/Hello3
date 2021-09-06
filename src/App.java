98242fce133d25de176036d7700f1e056d4a3366
Norbi <szucs.norb3rt0424@gmail.com>
2021-09-06 09:19:16 +0200

    modify github user

--- src/App.java
+++ src/App.java
@@ -1,3 +1,12 @@
+/*
+* File: App.java
+* Author: Szűcs Norbert
+* Copyright: 2021, Szűcs Norbert
+* Group: Szoft II/N
+* Date: 2021-09-06
+* Github: https://github.com/Szucs-Norbert/
+* Licenc: GNU GPL
+*/
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
